98cd9bfe759d7688178795120fa929e42f2924b1
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-02-23 13:31:17 -0300

    Exercicio 08

--- Módulo 02 - Semana 04 - Exercícios/script.js
+++ Módulo 02 - Semana 04 - Exercícios/script.js
@@ -1,1 +1,10 @@
 window.alert("Bem-vindo! Espero que goste do meu site.");
+
+let novoNomeUsuario = "Lucas Pedro";
+let novaDescricao = "Um desenvolvedor em desenvolvimento.";
+let nomeUsuarioElemento = document.querySelector("#perfil h2");
+let descricao = document.querySelector("#perfil p");
+
+nomeUsuarioElemento.textContent = novoNomeUsuario;
+
+descricao.textContent = novaDescricao;
