75be60a6a963395bb91c22629ae6c34f31cdfab6
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-02-23 14:12:11 -0300

    Exercício 10; Adicionado logos, adicionado botão de alerta, corrigido posições e cores.

--- Módulo 02 - Semana 04 - Exercícios/script.js
+++ Módulo 02 - Semana 04 - Exercícios/script.js
@@ -1,5 +1,4 @@
-window.alert("Bem-vindo! Espero que goste do meu site.");
-
+// Exercicio 08
 let novoNomeUsuario = "Lucas Pedro";
 let novaDescricao = "Um desenvolvedor em desenvolvimento.";
 let nomeUsuarioElemento = document.querySelector("#perfil h2");
@@ -8,3 +7,11 @@ let descricao = document.querySelector("#perfil p");
 nomeUsuarioElemento.textContent = novoNomeUsuario;
 
 descricao.textContent = novaDescricao;
+
+//Exercicio 10
+function mostrarAlerta() {
+    window.alert("Bem-vindo! Espero que goste do meu site.");
+}
+
+// Associando a função ao evento de clique do botão
+document.getElementById("btnAlerta").addEventListener("click", mostrarAlerta);
