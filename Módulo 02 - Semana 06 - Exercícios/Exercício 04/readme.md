00a457997159e5d753ff5835941b000b5d8e727f
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-03-09 21:59:27 -0300

    Semana 06 - Exercicio 04

---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 04/readme.md
@@ -0,0 +1,3 @@
+# Exercício 04 - Remover Elemento Filho
+
+- Remova um elemento filho de um elemento pai na página HTML
---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 04/script.js
@@ -0,0 +1,7 @@
+var container = document.querySelector('.container');
+
+var span = container.querySelector('span');
+
+container.removeChild(span);
+
+console.log('O elemento <span> foi removido.');
