e6a880bfe25677bb28b02150da9bea2bfdf6e347
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-03-09 21:47:27 -0300

    Semana 06 - Exercício 03

---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 03/readme.md
@@ -0,0 +1,3 @@
+# Exercício 03 - Criar e Adicionar Elemento
+
+- Crie um novo elemento e adicione-o como filho de outro elemento na página HTML.
---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 03/script.js
@@ -0,0 +1,7 @@
+var novoElemento = document.createElement('h2');
+
+novoElemento.innerText = 'Eu sou um novo elemento H2!';
+
+var container = document.querySelector('.container');
+
+container.appendChild(novoElemento);
