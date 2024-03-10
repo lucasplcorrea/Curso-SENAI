479b0185c885a67f6c73ea509b3b05c6ecdab6dc
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-03-09 23:53:46 -0300

    Exercicio 06

---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 06/readme.md
@@ -0,0 +1,3 @@
+# Exercício 06 - Alterar Estilo por Classe
+
+- Adicione uma nova classe a um elemento específico na página HTML.
---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 06/script.js
@@ -0,0 +1,5 @@
+let elementosClasse2 = document.querySelectorAll('.classe2');
+
+elementosClasse2.forEach(elemento => {
+    elemento.classList.add('classeEspecial');
+});
