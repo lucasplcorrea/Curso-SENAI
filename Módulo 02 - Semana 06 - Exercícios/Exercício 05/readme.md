992ca4500ad95803697e8035c641be3f1bee212f
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-03-09 22:15:06 -0300

    Exercício 05

---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 05/readme.md
@@ -0,0 +1,3 @@
+# Exercício 05 - Alterar Estilo por Classe
+
+- Altere o estilo de todos os elementos de uma página HTML que tenham uma determinada classe.
---
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 05/script.js
@@ -0,0 +1,5 @@
+let elementosClasse1 = document.querySelectorAll('.classe1');
+
+elementosClasse1.forEach(function(elemento) {
+    elemento.style.color = 'red'; 
+});
