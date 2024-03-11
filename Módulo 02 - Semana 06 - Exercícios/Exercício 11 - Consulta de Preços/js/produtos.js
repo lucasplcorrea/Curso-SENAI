cf5bf0248bbcf07a649c83ee0886fff242d458af
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-03-11 01:52:13 -0300

    Inclusão de novos produtos, reomção de informações relativas a imagens

--- Módulo 02 - Semana 06 - Exercícios/Exercício 11 - Consulta de Preços/js/produtos.js
+++ Módulo 02 - Semana 06 - Exercícios/Exercício 11 - Consulta de Preços/js/produtos.js
@@ -3,18 +3,100 @@ const produtos = [
         codigo: "001",
         nome: "Arroz",
         preco: 10.99,
-        imagem: "../img/arroz.png"
     },
     {
         codigo: "002",
         nome: "Feijão",
         preco: 8.50,
-        imagem: "../img/feijao.png"
     },
     {
         codigo: "003",
         nome: "Óleo de Soja",
         preco: 5.99,
-        imagem: "../img/oleo.png"
+    },
+    {
+        codigo: "004",
+        nome: "Açúcar",
+        preco: 3.49,
+    },
+    {
+        codigo: "005",
+        nome: "Farinha de Trigo",
+        preco: 4.99,
+    },
+    {
+        codigo: "006",
+        nome: "Sal",
+        preco: 2.00,
+    },
+    {
+        codigo: "007",
+        nome: "Café",
+        preco: 12.99,
+    },
+    {
+        codigo: "008",
+        nome: "Leite",
+        preco: 4.50,
+    },
+    {
+        codigo: "009",
+        nome: "Manteiga",
+        preco: 6.75,
+    },
+    {
+        codigo: "010",
+        nome: "Pão",
+        preco: 3.00,
+    },
+    {
+        codigo: "011",
+        nome: "Macarrão",
+        preco: 2.25,
+    },
+    {
+        codigo: "012",
+        nome: "Queijo",
+        preco: 8.99,
+    },
+    {
+        codigo: "013",
+        nome: "Presunto",
+        preco: 7.25,
+    },
+    {
+        codigo: "014",
+        nome: "Ovos",
+        preco: 5.00,
+    },
+    {
+        codigo: "015",
+        nome: "Tomate",
+        preco: 3.75,
+    },
+    {
+        codigo: "016",
+        nome: "Cebola",
+        preco: 2.50,
+    },
+    {
+        codigo: "017",
+        nome: "Alho",
+        preco: 2.00,
+    },
+    {
+        codigo: "018",
+        nome: "Banana",
+        preco: 3.99,
+    },
+    {
+        codigo: "019",
+        nome: "Maçã",
+        preco: 4.50,
+    },
+    {
+        codigo: "020",
+        nome: "Laranja",
+        preco: 3.25,
     },
 ];
