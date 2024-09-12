072b376c570d0c9c1a27882ac742cc48c57a8f5a
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-09-12 16:03:53 -0300

    Contexto e Autenticação

---
+++ Módulo 03/Exercício 01/contador/src/context/AuthContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthProvider, AuthContext };
--- Módulo 03/Exercício 01/contador/src/main.jsx
+++ Módulo 03/Exercício 01/contador/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
